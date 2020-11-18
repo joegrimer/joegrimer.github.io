efb2f504f041b3e68f71b43d9c621556c40a4550
Joseph Grimer <j.grimer@gblabs.co.uk>
2020-11-18 12:33:21 +0000

    .

--- project_euler/27.c
+++ project_euler/27.c
@@ -3,7 +3,7 @@ Euler discovered the remarkable quadratic formula:
 
 n^2 + n + 41
 
-It turns out that the formula will produce 40 primes for the consecutive integer values 0 <= n <= 36. However, when n=40, 40^2 + 40 + 41 = 40(40+1)+41 is divisible by 41, and certainly when n=41, 41^2 + 41 + 41 is clearly divisible by 41.
+It turns out that the formula will produce 40 primes for the consecutive integer values 0 <= n <= 39. However, when n=40, 40^2 + 40 + 41 = 40(40+1)+41 is divisible by 41, and certainly when n=41, 41^2 + 41 + 41 is clearly divisible by 41.
 
 The incredible formula n^2 - 79n + 1601 was discovered, which produces 80 primes for the consecutive values 0 <= n <= 79. The product of the coefficients, −79 and 1601, is −126479.
 
@@ -20,9 +20,6 @@ Find the product of the coefficients, a and b, for the quadratic expression that
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define A_MAX 3
-#define B_MAX 3
-#define N_MAX 79
 
 int run_formula(int n, int a, int b) {
     // n^2 + an + b
@@ -34,7 +31,7 @@ int is_prime(int subject) {
         int quotient = subject/p_factor; // will remove remainder
         int product = quotient*p_factor;
         if (product==subject) // then it's a factor
-            return 0; // not prime
+            return p_factor; // not prime
         if (p_factor*p_factor >= subject) // eliminating innefficiency
             break;
     }
@@ -44,14 +41,27 @@ int is_prime(int subject) {
 int main() {
     printf("Start of Program.\n\n"); 
    
-    int a=1, b=41;
-    //for (int a=0;a<A_MAX;a++) {
-    //for (int b=0;b<B_MAX;b++) {
-    for (int n=0;n<=N_MAX;n++) {
-    int result = run_formula(n, a, b);
-    printf("%d^2 + %d*%d + %d = %d (prime? %d)\n", n, a, n, b, result, is_prime(result));
+    int best_a=0, best_b=0, best_n=0;
+    int a=-999, b=-996;
+    for (a=-999;a<=999;a++) { for (b=-1000;b<=1000;b++) {
+    for (int n=0;1;n++) {
+        int result = run_formula(n, a, b);
+        //printf("%d^2 + %d*%d + %d = %d (prime? %d)\n", n, a, n, b, result, is_prime(result));
+        if (is_prime(result)!=1) {
+
+            //printf("max for a=%d and b=%d was %d\n", a, b, (n-1));
+            if ((n-1)>best_n) {
+                best_a=a;
+                best_b=b;
+                best_n=(n-1);
+            }
+            break;
+        }
     }
-    printf("-\n");
+    }}
+    printf("best_a %d best_b %d best_n %d\n", best_a, best_b, best_n);
+    printf("product of a and b is: %d\n",(best_a*best_b));
+
 
     printf("\nEnd of Program.\n"); 
 }
