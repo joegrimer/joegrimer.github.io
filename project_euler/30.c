bacdcf64205869d536ceb3ccd99dce169386da2d
joegrimer@gmail.com <joegrimer@gmail.com>
2020-11-25 14:40:12 +0000

    .

---
+++ project_euler/30.c
@@ -0,0 +1,24 @@
+/*
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MIN_CYCLE 1
+#define MAX_CYCLE 1000
+
+int run_formula(int n) {
+    // 
+
+}
+
+int main() {
+    printf("\n\nStart of Program."); 
+
+    
+
+    printf("\n\nEnd of Program."); 
+}
+
+
+
--- project_euler/test.c
+++ project_euler/test.c
@@ -1,19 +1,9 @@
-/* at top */
 #include <stdlib.h>
 #include <stdio.h>
-#include <gmp.h>
+#include <string.h>
 
 int main() {
-/* definition */
-mpz_t *A;
-
-/* initialization of A */
-A = (mpz_t *) malloc(10000 * sizeof(mpz_t));
-if (NULL == A) {
-    printf("ERROR: Out of memory\n");
-        return 1;
-        }
-
-        /* no longer need A */
-        free(A);
-        }
+   if (strcmp("ab","ab")==0)
+      printf("matches");
+   else printf("not matches");
+}
