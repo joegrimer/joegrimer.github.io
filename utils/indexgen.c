ba6fd638b90c51624239c272ca740a28a170d9b8
Joseph Grimer <j.grimer@gblabs.co.uk>
2021-03-28 00:41:46 +0000

    updated front html

--- utils/indexgen.c
+++ utils/indexgen.c
@@ -1,7 +1,8 @@
 #include <stdio.h> 
 #include <dirent.h> 
 #include <string.h> 
-  
+#include <sys/stat.h>
+
 void do_dir(char *path) 
 { 
    struct dirent *de;  // Pointer for directory entry 
@@ -16,17 +17,32 @@ void do_dir(char *path)
       return; 
    } 
 
+   struct stat stbuf;
    while ((de = readdir(dr)) != NULL) {
       strcpy(file_name, de->d_name);
-      if (strcmp(file_name, ".")==0 || strcmp(file_name, "..")==0) continue;
-      printf("%s\n", file_name); 
+      if (file_name[0] == *".") continue;
+      
+      stat(file_name, &stbuf); // livin on the edge
+      if ((stbuf.st_mode & S_IFMT) == S_IFDIR) {
+          printf("<p>%s/%s/</p>\n", path, file_name);
+          char sub_file_name[1024];
+          strcpy(sub_file_name, path);
+          strcat(sub_file_name, "/");
+          strcat(sub_file_name, file_name);
+          do_dir(sub_file_name);
+      } else {
+          printf("<p><a href='%s/%s'>%s/%s</a></p>\n", path, file_name, path, file_name);
+      }
    }
   
    closedir(dr);   
    return; 
-} 
+}
 
 int main(void) 
 { 
+   printf("<!doctype html><html><head><link rel='stylesheet' href='style.css' /><title>Hello There!</title><body>");
+   printf("An inelegant dump of everything on this git repository");
    do_dir(".");
+   printf("</body></html>");
 } 
