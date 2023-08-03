4d22548352aa6677d894538d04a0524076b7e9c5
Joseph.Grimer <joseph.grimer@ingresso.co.uk>
2023-08-03 16:33:09 +0100

    Print nn

--- neuralnet_geneticalg/nn_ga.py
+++ neuralnet_geneticalg/nn_ga.py
@@ -11,29 +11,49 @@ Task:
     - Play against it
 """
 from pprint import pprint
+import random
 
 INPUTS = 2
 OUTPUTS = 2
-LAYERS = 1  # middle layers
+COLUMNS = 2  # middle layers
 HEIGHT = INPUTS
-neural_network = {}
+nn = []
+
+def starting_node_weight():
+    return random.randint(1, 9)
 
 def main():
     print("making nn")
-    nid = 2
-    for layer in range(0, LAYERS):
+    nid = 0
+    for column_no in range(0, COLUMNS):
+        nn.append({})
         for row in range(0, HEIGHT):
-            neural_network[nid] = {
-                "in": 12,
-                "outs": {
-                    4: 11,
-                }
+            nn[column_no][nid] = {
+                "in_weights": {},
+                "out_weight": starting_node_weight(),
             }
+            if column_no != 0:
+                nn[column_no][nid]["in_weights"] = {nid: starting_node_weight() for nid in nn[column_no - 1].keys()}
             nid += 1
 
     print("print nn")
-    for nid, node in neural_network.items():
-        print(f"{node['in']}->{nid}->{node['outs']}\n")
+    rotated_screen = []
+    c_id = 0
+    for column in nn:
+        r_id = 0
+        for nid, node in column.items():
+            if r_id > len(rotated_screen)-1:
+                rotated_screen.append([])
+            if c_id > len(rotated_screen[r_id])-1:
+                rotated_screen[r_id].append("null")
+            # print("rotated_sceen", rotated_screen)
+            # print("r_id", r_id)
+            # print("c_id", c_id)
+            rotated_screen[r_id][c_id] = f"{node['in_weights']}->[{nid}]->{node['out_weight']}"
+            r_id += 1
+        c_id += 1
+    for row in rotated_screen:
+        print("\t|\t".join(row))
 
 
 if __name__ == '__main__':
