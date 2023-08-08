c82e234be900c3e07390857dff40a6d9ce7485a2
Joseph.Grimer <joseph.grimer@ingresso.co.uk>
2023-08-08 11:59:23 +0100

    Play with numpy and rust

---
+++ manuel_chatbot/target/debug/manuel_chatbot.d
@@ -0,0 +1,1 @@
+/Users/joseph.grimer/src/joegrimer.github.io/manuel_chatbot/target/debug/manuel_chatbot: /Users/joseph.grimer/src/joegrimer.github.io/manuel_chatbot/src/main.rs
---
+++ numpy/n1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+ary1 = np.empty(99)  # random mem access?
+
+print(ary1)
+
+print("np.linspace(0, 10, num=5)")
+print(np.linspace(0, 10, num=5))
+
+
