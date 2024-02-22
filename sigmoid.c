b05ac6da6d2f33b660146db87c748146315544f8
Joseph.Grimer <joseph.grimer@ingresso.co.uk>
2024-02-22 15:27:07 -0300

    Dumb sigmoid func

--- sigmoid.c
+++ sigmoid.c
@@ -9,7 +9,9 @@ int sigmoid(int a) {
 }
 
 int main() {
- for (int i=-240;i < 240; i++) {
-  printf("%d -> %d\n", i, sigmoid(i));
+ int examples[] = {-999,-200,-80,-20,-4,-3,-2,-1,0,4,6,40,56,122,567,1234};
+ for (int i=0;i < 16; i++) {
+  int val=examples[i];
+  printf("%d -> %d\n", val, sigmoid(val));
  }
 }
