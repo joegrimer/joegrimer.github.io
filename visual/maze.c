7eb9b3974a5b5564fc60347580becf3681c74674
Joseph.Grimer <joseph.grimer@ingresso.co.uk>
2024-02-23 15:26:02 -0300

    Work on maze

--- visual/maze.c
+++ visual/maze.c
@@ -1,12 +1,51 @@
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define WIDTH 60
+#define HEIGHT 30
+
+int grid[WIDTH][HEIGHT] = {};
+void print_maze();
+int finishable(int x, int y);
 
 int main() {
-    for(int i=0;i<100;i++) {
-        for(int j=0;j<100;j++) {
-            
-            if (rand() %7) printf(" ");
-            else printf("O");
+   printf("Welcome to maze. Your goal: get from top left to bottom right\n\n");
+   srand(time(NULL));   // Initialization, should only be called once.
+   int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+
+   int i, j;
+
+   for(i=0;i<WIDTH;i++)
+       for(j=0;j<WIDTH;j++) grid[i][j] = (rand() % 7 == 1);
+
+   print_maze();
+
+   printf("Finishable? %d\n", finishable(0, 0));
+   printf("Fin\n");
+}
+
+int finishable(int x, int y) {
+   if (grid[x][y] == '#') return 0;
+   else if (x == WIDTH) return 1;
+   else return finishable(x+1, y);
+}
+
+void print_maze() {
+   printf("Start\n");
+   printf("V");
+    for(int i=0;i<WIDTH-1;i++) printf("_");
+    printf("\n");
+    for(int i=0;i<HEIGHT;i++) {
+        for(int j=0;j<WIDTH;j++) {
+            if (grid[i][j]) printf("#");
+            else printf(" ");
         }
         printf("\n");
     }
+    for(int i=0;i<WIDTH-1;i++) printf("-");
+   printf("^\n");
+    for(int i=0;i<WIDTH-3;i++) printf(" ");
+   printf("end\n\n");
 }
