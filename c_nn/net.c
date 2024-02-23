5b764d3a9fc67ed73eb5b788b223c968cd0e82cf
Joseph.Grimer <joseph.grimer@ingresso.co.uk>
2024-02-23 14:42:56 -0300

    Smidge matrix stuff

--- c_nn/net.c
+++ c_nn/net.c
@@ -8,12 +8,14 @@
 #define NO_PERLAYER 3
 #define PER_NEURON_NO 2
 #define NO_OUTPUTS 2
+#define NN_MAX 144
 
 int inputs[NO_INPUTS] = {};
 // each neuron has in weight, gate, and out weight
 int neurons[NO_LAYERS][NO_PERLAYER][PER_NEURON_NO] = {};
 int outputs[NO_OUTPUTS] = {};
 
+void init_nn();
 void print_nn();
 
 int main() {
@@ -23,8 +25,25 @@ int main() {
 
    printf("rand max is %d\n", RAND_MAX);
    printf("rand is %d\n", r);
+   init_nn();
    print_nn();
    printf("Fin\n");
+}
+
+void init_nn() {
+   int i, j, k;
+    for (i=0; i<NO_INPUTS;i++) {
+       inputs[i] = 0;
+    }
+    for (i=0; i<NO_LAYERS;i++) {
+    for (j=0; j<NO_PERLAYER;j++) {
+        neurons[i][j][0] = rand() % NN_MAX;
+        neurons[i][j][1] = rand() % NN_MAX;
+    }
+    }
+    for (i=0; i<NO_OUTPUTS;i++) {
+       outputs[i] = 0;
+    }
 }
 
 void print_nn() {
