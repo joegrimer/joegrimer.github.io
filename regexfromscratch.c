615697d2ed48b0c9b1b170551e60a04d3f43caeb
Joseph.Grimer <joseph.grimer@ingresso.co.uk>
2024-02-23 14:58:27 -0300

    Tidy

--- regexfromscratch.c
+++ regexfromscratch.c
@@ -12,12 +12,12 @@ int main()
 
    int sample_len = strlen(sample);
    printf("string len: %d\n", sample_len);
-   
+
    int i;
    for(i=0;i<sample_len;i++) {
       char sample_letter = sample[i];
       char regex_letter = regex[i];
-      
+
       printf("sample regex: %d %d\n", sample_letter, regex_letter);
       if(regex_letter == '.') continue;
       else if(sample_letter != regex_letter) return 1;
