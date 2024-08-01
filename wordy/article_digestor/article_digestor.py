cbdcb3d28e1488dd762f8f8a1b060bc849eaa7c6
joegrimer <joegrimer@gmail.com>
2024-08-01 15:51:39 +0100

    Fixing what I just discovered is markov text

--- wordy/article_digestor/article_digestor.py
+++ wordy/article_digestor/article_digestor.py
@@ -5,6 +5,8 @@
 
 ############################## Libraries ######################################
 
+import random
+
 ############################## Global Vars ####################################
 
 ############################## Main Functions #################################
@@ -14,6 +16,159 @@ def main():
 	print("Welcome to Ad - son of Ai.")
 	article = "John ran across the green plane. He didn't like the grass, but he loved the sun, the rain and the wind in his teeth. There was a tree at the end of the field. The tree spoke to him. The tree told him to come. The tree did not want to eat him. But he wanted to climb the tree. However, deep in the grass was hidden a snake, ready to bite those who wished to climb the tree. The tree has long curved branches. The snake was black with green stripes. You are alone. He wants to see you. The boy had red trousers, and a green shirt. The boy was ignorant. The tree was strong. The snake was malicious. The snake was actually hungry. Maybe you are hungry. Maybe this story sounds silly. It's not supposed to be art. It's just for my little experiment, you see."
 	article = "Theresa May has backed away from a pledge to require companies to put worker representatives on boards. Speaking to the CBI's annual conference, Mrs May said firms would not be forced to adopt the controversial proposal. 'This is not about mandating works councils, or the direct appointment of workers or trade union representatives on boards,' she said. Mrs May's pledge had met with a cool response from business lobby groups.The prime minister said there were 'other routes' that used existing board structures, but supplemented by advisory councils or panels. 'It will be a question of finding the model that works,' she said. Mrs May promised to shake-up corporate governance as part of her Conservative Party leadership campaign in July, and repeated the promise at last month's party conference when she said she planned to have 'not just consumers represented on company boards, but workers as well'. Asked if she had dropped plans for the direct appointment of workers on boards, Mrs May said she had 'clarified' that 'we want workers' representation on boards', but 'there are a number of ways in which that can be achieved.'She said the government would soon publish its plans to reform corporate governance, which would cover firms' accountability to shareholders, executive pay as well as proposals to ensure employees' voices were heard.In her speech to business leaders, Mrs May also pledged an extra £2bn a year in funding for scientific research and development by 2020."
+	article="""
+The gallows in my garden, people say,
+Is new and neat and adequately tall;
+I tie the noose on in a knowing way
+As one that knots his necktie for a ball;
+But just as all the neighbours--on the wall--
+Are drawing a long breath to shout "Hurray!"
+The strangest whim has seized me. . . . After all
+I think I will not hang myself to-day.
+
+To-morrow is the time I get my pay--
+My uncle's sword is hanging in the hall--
+I see a little cloud all pink and grey--
+Perhaps the rector's mother will not call-- I fancy that I heard from Mr. Gall
+That mushrooms could be cooked another way--
+I never read the works of Juvenal--
+I think I will not hang myself to-day.
+
+The world will have another washing-day;
+The decadents decay; the pedants pall;
+And H.G. Wells has found that children play,
+And Bernard Shaw discovered that they squall,
+Rationalists are growing rational--
+And through thick woods one finds a stream astray
+So secret that the very sky seems small--
+I think I will not hang myself to-day.
+
+Envoi
+
+Prince, I can hear the trumpet of Germinal,
+The tumbrils toiling up the terrible way;
+Even to-day your royal head may fall,
+I think I will not hang myself to-day.
+
+This much, O heaven—if I should brood or rave,
+Pity me not; but let the world be fed,
+Yea, in my madness if I strike me dead,
+Heed you the grass that grows upon my grave.
+
+If I dare snarl between this sun and sod,
+Whimper and clamour, give me grace to own,
+In sun and rain and fruit in season shown,
+The shining silence of the scorn of God.
+
+Thank God the stars are set beyond my power,
+If I must travail in a night of wrath,
+Thank God my tears will never vex a moth,
+Nor any curse of mine cut down a flower.
+
+Men say the sun was darkened: yet I had
+Thought it beat brightly, even on—Calvary:
+And He that hung upon the Torturing Tree
+Heard all the crickets singing, and was glad.
+
+When fishes flew and forests walked
+And figs grew upon thorn,
+Some moment when the moon was blood,
+Then surely I was born;
+
+With monstrous head and sickening cry
+And ears like errant wings,
+The devil's walking parody
+On all four-footed things.
+
+The tattered outlaw of the earth,
+Of ancient crooked will;
+Starve, scourge, deride me: I am dumb,
+I keep my secret still.
+
+Fools! For I also had my hour;
+One far fierce hour and sweet:
+There was a shout about my ears,
+And palms before my feet.
+
+The line breaks and the guns go under,
+The lords and the lackeys ride the plain;
+I draw deep breaths of the dawn and thunder,
+And the whole of my heart grows young again.
+For our chiefs said 'Done,' and I did not deem it;
+Our seers said 'Peace,' and it was not peace;
+Earth will grow worse till men redeem it,
+And wars more evil, ere all wars cease.
+But the old flags reel and the old drums rattle,
+As once in my life they throbbed and reeled;
+I have found my youth in the lost battle,
+I have found my heart on the battlefield.
+For we that fight till the world is free,
+We are not easy in victory:
+We have known each other too long, my brother,
+And fought each other, the world and we.
+
+And I dream of the days when work was scrappy,
+And rare in our pockets the mark of the mint,
+When we were angry and poor and happy,
+And proud of seeing our names in print.
+For so they conquered and so we scattered,
+When the Devil road and his dogs smelt gold,
+And the peace of a harmless folk was shattered;
+When I was twenty and odd years old.
+When the mongrel men that the market classes
+Had slimy hands upon England's rod,
+And sword in hand upon Afric's passes
+Her last Republic cried to God.
+For the men no lords can buy or sell,
+They sit not easy when all goes well,
+They have said to each other what naught can smother,
+They have seen each other, our souls and hell.
+
+It is all as of old, the empty clangour,
+The Nothing scrawled on a five-foot page,
+The huckster who, mocking holy anger,
+Painfully paints his face with rage.
+And the faith of the poor is faint and partial,
+And the pride of the rich is all for sale,
+And the chosen heralds of England's Marshal
+Are the sandwich-men of the Daily Mail,
+And the niggards that dare not give are glutted,
+And the feeble that dare not fail are strong,
+So while the City of Toil is gutted,
+I sit in the saddle and sing my song.
+For we that fight till the world is free,
+We have no comfort in victory;
+We have read each other as Cain his brother,
+We know each other, these slaves and we.
+
+Before the Roman came to Rye or out to Severn strode,
+The rolling English drunkard made the rolling English road.
+A reeling road, a rolling road, that rambles round the shire,
+And after him the parson ran, the sexton and the squire;
+A merry road, a mazy road, and such as we did tread
+The night we went to Birmingham by way of Beachy Head.
+
+I knew no harm of Bonaparte and plenty of the Squire,
+And for to fight the Frenchman I did not much desire;
+But I did bash their baggonets because they came arrayed
+To straighten out the crooked road an English drunkard made,
+Where you and I went down the lane with ale-mugs in our hands,
+The night we went to Glastonbury by way of Goodwin Sands.
+
+His sins they were forgiven him; or why do flowers run
+Behind him; and the hedges all strengthening in the sun?
+The wild thing went from left to right and knew not which was which,
+But the wild rose was above him when they found him in the ditch.
+God pardon us, nor harden us; we did not see so clear
+The night we went to Bannockburn by way of Brighton Pier.
+
+My friends, we will not go again or ape an ancient rage,
+Or stretch the folly of our youth to be the shame of age,
+But walk with clearer eyes and ears this path that wandereth,
+And see undrugged in evening light the decent inn of death;
+For there is good news yet to hear and fine things to be seen,
+Before we go to Paradise by way of Kensal Green.
+	"""
 	print("Article has been chosen") # (example 2 from BBC News)")
 
 	words = article.split(" ")
@@ -34,8 +189,8 @@ def main():
 
 	print(wordConnections)
 	print("\n--------------------------------------\n")
-	output = ["John"] # eventually random caps word from dict
-	nextSearch = wordConnections["John"][-1]
+	output = [random.choice(list(wordConnections.keys()))] # eventually random caps word from dict
+	nextSearch = wordConnections[output[0]][-1]
 	for i in range(0,98):
 		if nextSearch in wordConnections:
 			output.append(nextSearch)
