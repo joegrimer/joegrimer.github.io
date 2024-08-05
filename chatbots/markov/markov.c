cbd3f3118123cdb3b658d6577829cd41b664c673
joegrimer <joegrimer@gmail.com>
2024-08-05 18:19:27 +0100

    Progress on chatbot markov ish

--- chatbots/markov/markov.c
+++ chatbots/markov/markov.c
@@ -26,15 +26,53 @@ int main() {
     return 0;
 }
 
+char *random_phrase_from_memory() {
+    FILE *fp = fopen("memory.txt", "r");
+
+    char file_char = '?';
+    while (file_char != EOF) {
+        file_char=getc(fp);
+        printf("z %c<>%c\n", *line_ptr, file_char);
+        if ( *line_ptr != file_char ) {
+            line_ptr = line;
+            while (file_char != '\n' && file_char != EOF) {
+                file_char=getc(fp);
+            }
+        } else if ( file_char=='\n' ) {
+            line_ptr = line; // reset
+            printf("That line was found in memory! %s\n", line);
+        } else
+            line_ptr++;
+    }
+
+    fclose(fp);
+
+}
+
 void record_to_memory_if_new(char *line) {
 
     FILE *fp = fopen("memory.txt", "r");
 
-    char *lineP = line;
-    for (;*fp!=EOF;fp++) {
+    char *line_ptr = line;
+    char file_char = '?';
+    while (file_char != EOF) {
+        file_char=getc(fp);
+        printf("z %c<>%c\n", *line_ptr, file_char);
+        if ( *line_ptr != file_char ) {
+            line_ptr = line;
+            while (file_char != '\n' && file_char != EOF) {
+                file_char=getc(fp);
+            }
+        } else if ( file_char=='\n' ) {
+            line_ptr = line; // reset
+            printf("That line was found in memory! %s\n", line);
+        } else
+            line_ptr++;
     }
 
     fclose(fp);
+
+    printf("print line: %s\n", line);
 
     FILE *fp2 = fopen("memory.txt", "a");
 
