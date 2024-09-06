5a9a73bb26c7d15a28a805991900bc1c7cc0c6f9
joegrimer <joegrimer@gmail.com>
2024-09-06 15:53:46 +0100

    Undescribed commit at Fri Sep  6 15:53:46 BST 2024

--- unicode_explorer/explorer.c
+++ unicode_explorer/explorer.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
 
 void pf(char *_s) {printf("%s\n",_s); }
 int main() {
 
+	pf("Ascii");
 	char manchar=0;
 	for (int i=0;i<=258;i++) {
-		printf("%c \n", manchar);
+		printf("%c ", manchar);
 		manchar++;
 	}
+	pf("\nUnicoder");
+
+	printf("\xE2\x98\xA0 \xE2\x98\xA0 bob");
+	pf("end");
 
 }
