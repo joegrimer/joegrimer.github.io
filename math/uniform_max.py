664a76927e30a2ed421de431a1ed543674066e3c
joegrimer <joegrimer@gmail.com>
2024-09-16 17:47:54 +0100

    Undescribed commit at Mon Sep 16 17:47:54 BST 2024

--- math/uniform_max.py
+++ math/uniform_max.py
@@ -2,41 +2,14 @@
 import matplotlib.pyplot as plt
 
 MAX_RANGE = 144
-RRANGE = 99999
 
 numbers = []
-
 
 for i in range(1, MAX_RANGE):
     for j in range(1, MAX_RANGE):
         numbers.append(
-            i*j
+            max((i, j))
         )
 
-plt.hist(numbers, bins=MAX_RANGE)
+plt.hist(numbers, bins=250)
 plt.show()
-
-numbers = []
-print("normal")
-
-for i in range(1, RRANGE):
-    numbers.append(i)
-    # numbers.append(random.randint(1, MAX_RANGE))
-
-plt.hist(numbers, bins=MAX_RANGE)
-plt.show()
-
-#print("now with max")
-
-#numbers = []
-#for i in range(1, RRANGE):
-    #numbers.append(max((
-    #    random.randint(1, MAX_RANGE),
-    #    random.randint(1, MAX_RANGE),
-    #    ))
-    #)
-
-#plt.hist(numbers, bins=MAX_RANGE)
-#plt.show()
-
-print("now squared")
